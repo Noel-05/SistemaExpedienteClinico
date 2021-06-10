48a7ae7bde02df6a369ff63f80ae13d0a12aed09
lol9519 <32627694+lol9519@users.noreply.github.com>
2021-06-10 16:13:39 -0600

    Create TratamientoMedico.groovy

---
+++ grails-app/domain/sistemaclinico/TratamientoMedico.groovy
@@ -0,0 +1,14 @@
+package sistemaclinico
+
+class TratamientoMedico {
+    Long id
+    Date fechaAsignado
+    String comentario
+    
+    static mappedBy=[]
+    static constraints = {
+    }
+    static mapping={
+        
+    }
+}
